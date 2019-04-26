f4a6f96cde7febb86bbb3306943c807add10e528
Jose Felix <jfelix000@west-mec.org>
2019-04-25 17:21:24 -0700

    Final commit
    Ready for Review

--- mas_reg2.js
+++ mas_reg2.js
@@ -21,3 +21,18 @@
 
 */
 
+window.onload = writeSessionValues;
+
+
+function writeSessionValues() {
+      
+      document.getElementById("regName").textContent = sessionStorage.confName;
+      document.getElementById("regGroup").textContent = sessionStorage.confGroup;
+      document.getElementById("regEmail").textContent = sessionStorage.confMail;
+      document.getElementById("regPhone").textContent = sessionStorage.confPhone;
+      document.getElementById("regSession").textContent = sessionStorage.confSession;
+      document.getElementById("regBanquet").textContent = sessionStorage.confBanquet;
+      document.getElementById("regPack").textContent = sessionStorage.confPack;
+
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.confTotal;
+}
--- mas_register.js
+++ mas_register.js
@@ -28,34 +28,78 @@
 
 */
 window.addEventListener("load", function () {
-      calcCart();
-      // lets sessiontest run whenever the regSubmit button is clicked
+
       document.getElementById("regSubmit").onclick = sessionTest;
-      // runs calcCart every focus change
+      // this is to get the calcCart function run whenever something is focused
       document.getElementById("fnBox").onblur = calcCart;
       document.getElementById("lnBox").onblur = calcCart;
       document.getElementById("groupBox").onblur = calcCart;
       document.getElementById("mailBox").onblur = calcCart;
       document.getElementById("phoneBox").onblur = calcCart;
-      document.getElementById("banquentBox").onblur = calcCart;
-      // runs calcCart when sessionBox is changed
+      document.getElementById("banquetBox").onblur = calcCart;
+
       document.getElementById("sessionBox").onchange = calcCart;
-      //runs calcCart when mediaCB is changed
+
       document.getElementById("mediaCB").onclick = calcCart;
+
+      calcCart();
 });
 
-function sessiontest () {
-      var session = document.getElementById("sessionBox");
-      if (indexOf(session) !== -1) { 
-            session.setCustomValidity("Select a session package");
+
+function sessionTest() {
+      var confSession = document.getElementById("sessionBox");
+      if (confSession === -1) {
+            confSession.setCustomValidity("Select a Session Package");
       } else {
-            session.setCustomValidity("");
+            confSession.setCustomValidity("");
       }
 }
 
+
 function calcCart() {
       sessionStorage.confName = document.forms.regForm.elements.fnBox.value + " " + document.forms.regForm.elements.lnBox.value;
+      // this is to store the respected ids and the values inserted
       sessionStorage.confGroup = document.forms.regForm.elements.groupBox.value;
       sessionStorage.confMail = document.forms.regForm.elements.mailBox.value;
-      sessionStorage.confPhone = document.forms.regForm.elements.phoneNumber.value;
+      sessionStorage.confPhone = document.forms.regForm.elements.phoneBox.value;
+      sessionStorage.confBanquet = document.forms.regForm.elements.banquetBox.value;
+
+      sessionStorage.confBanquetCost = sessionStorage.confBanquet * 55;
+      // This is to get the sessionBox id set and the selectedIndex is the attribute
+      var selectedIndex = document.getElementById("sessionBox").selectedIndex;
+
+      if (selectedIndex != -1) {
+            sessionStorage.confSession = document.forms.regForm.elements.sessionBox[selectedIndex].text;
+            sessionStorage.confSessionCost = document.forms.regForm.elements.sessionBox[selectedIndex].value;
+      } else {
+            sessionStorage.confSession = "";
+            sessionStorage.confSessionCost = 0;
+      }
+
+      
+      if (document.forms.regForm.elements.mediaCB.checked) {
+            sessionStorage.confPack = "yes";
+            sessionStorage.confPackCost = 115;
+      } else {
+            sessionStorage.confPack = "no";
+            sessionStorage.confPackCost = 0;
+      }
+      sessionStorage.confTotal = parseFloat(sessionStorage.confBanquetCost) + parseFloat(sessionStorage.confSessionCost) + parseFloat(sessionStorage.confPackCost);
+
+      writeSessionValues();
+}
+
+
+
+function writeSessionValues() {
+      // set the text of the items in the slots to the right of the website with sessionStorage
+      document.getElementById("regName").textContent = sessionStorage.confName;
+      document.getElementById("regGroup").textContent = sessionStorage.confGroup;
+      document.getElementById("regEmail").textContent = sessionStorage.confMail;
+      document.getElementById("regPhone").textContent = sessionStorage.confPhone;
+      document.getElementById("regSession").textContent = sessionStorage.confSession;
+      document.getElementById("regBanquet").textContent = sessionStorage.confBanquet;
+      document.getElementById("regPack").textContent = sessionStorage.confPack;
+
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.confTotal;
 }
